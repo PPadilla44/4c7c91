b278462f31466655c9d9f21d71f462b9de231588
Pablo Padilla <Padilla.Pablo1998@gmail.com>
2021-12-13 20:21:38 -0800

    changed messages to order by createdAt ascending

--- server/routes/api/conversations.js
+++ server/routes/api/conversations.js
@@ -19,7 +19,8 @@ router.get("/", async (req, res, next) => {
         },
       },
       attributes: ["id"],
-      order: [[Message, "createdAt", "DESC"]],
+      // Order by ascending
+      order: [[Message, "createdAt", "ASC"]],
       include: [
         { model: Message, order: ["createdAt", "DESC"] },
         {
@@ -68,7 +69,8 @@ router.get("/", async (req, res, next) => {
       }
 
       // set properties for notification count and latest message preview
-      convoJSON.latestMessageText = convoJSON.messages[0].text;
+      // Change message index to last in array
+      convoJSON.latestMessageText = convoJSON.messages[convoJSON.messages.length - 1].text;
       conversations[i] = convoJSON;
     }
 
