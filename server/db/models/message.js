ec55a34801b42d7bd64b625e9d71e766bbbaf1e6
Pablo Padilla <Padilla.Pablo1998@gmail.com>
2021-12-16 17:15:27 -0800

    Added isRead attribute to Messages

--- server/db/models/message.js
+++ server/db/models/message.js
@@ -1,5 +1,6 @@
 const Sequelize = require("sequelize");
 const db = require("../db");
+const { Op } = require("sequelize");
 
 const Message = db.define("message", {
   text: {
@@ -10,6 +11,27 @@ const Message = db.define("message", {
     type: Sequelize.INTEGER,
     allowNull: false,
   },
+  isRead: {
+    type: Sequelize.BOOLEAN,
+    allowNull: true,
+  }
 });
+
+Message.updateMessages = async function (messages) {
+  const ids = messages.map((msg => msg.id))
+  const updatedMessages = await Message.update(
+    {
+      isRead: true
+    },
+    {
+      where: {
+        id: {
+          [Op.in]: ids
+        }
+      }
+    }
+  )
+  return updatedMessages;
+}
 
 module.exports = Message;
--- server/routes/api/messages.js
+++ server/routes/api/messages.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const { Conversation, Message } = require("../../db/models");
 const onlineUsers = require("../../onlineUsers");
 
+
 // expects {recipientId, text, conversationId } in body (conversationId will be null if no conversation exists yet)
 router.post("/", async (req, res, next) => {
   try {
@@ -9,11 +10,12 @@ router.post("/", async (req, res, next) => {
       return res.sendStatus(401);
     }
     const senderId = req.user.id;
-    const { recipientId, text, conversationId, sender } = req.body;
+    // Adding read status
+    const { recipientId, text, conversationId, sender, isRead = false } = req.body;
 
     // if we already know conversation id, we can save time and just add it to message and return
     if (conversationId) {
-      const message = await Message.create({ senderId, text, conversationId });
+      const message = await Message.create({ senderId, text, conversationId, isRead });
       return res.json({ message, sender });
     }
     // if we don't have conversation id, find a conversation to make sure it doesn't already exist
@@ -36,8 +38,22 @@ router.post("/", async (req, res, next) => {
       senderId,
       text,
       conversationId: conversation.id,
+      isRead,
     });
     res.json({ message, sender });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/update", async (req, res, next) => {
+  try {
+    if (!req.user) {
+      return res.sendStatus(401);
+    }
+    await Message.updateMessages(req.body)
+    
+    res.json(MSG = "SUCCESS");
   } catch (error) {
     next(error);
   }
