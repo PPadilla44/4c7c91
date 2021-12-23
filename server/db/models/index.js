a170945ef0c8a72483bf81b6603b3e0aa225b087
PPadilla44 <Padilla.pablo1998@gmail.com>
2021-12-23 12:14:21 -0800

    Added many to many relationship

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -4,9 +4,8 @@ const Message = require("./message");
 
 // associations
 
-User.hasMany(Conversation);
-Conversation.belongsTo(User, { as: "user1" });
-Conversation.belongsTo(User, { as: "user2" });
+User.belongsToMany(Conversation, {through: "conversation_has_users"});
+Conversation.belongsToMany(User, { as: "users", through: "conversation_has_users" });
 Message.belongsTo(Conversation);
 Conversation.hasMany(Message);
 
